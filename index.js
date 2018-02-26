6b19fa11c41943935b4e9fdeb047e51d569d8fcb
mklarman <mklarman@hotmail.com>
2018-02-25 23:50:02 -0500

    final product

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var chosenWord = choices[puzzleWord]
 var checkWord = chosenWord.split("")
 var playerLife = 4
 var wordLife = checkWord.length
-var winMessage = 'You won. I demand a rematch!'
-var loseMessage = 'You are really stupid.'
+var winMessage = "What? You think you're better than me? You solved the word, so what?  You think that makes you better than me? Think again, pal."
+var loseMessage = 'This was actually an aptitude test and our software shows you best fit for employment is in an entry level fast food position.'
 var myBool = false
 var lettersInPuzzle;
 
@@ -18,7 +18,9 @@ var usedLetters = document.getElementById('usedLetters')
 var playerMsg = document.getElementById('playerMsg')
 var lifeLeft = document.getElementById('lifeleft')
 var hiddenLetters = document.createElement('div')
-var answerLetter; 
+var answerLetter;
+var pic1 = document.getElementById('pic1')
+var pic2 = document.getElementById('pic2') 
 hiddenLetters.setAttribute('class', 'letter')
 
 hintBox.innerHTML = hints[puzzleWord]
@@ -26,28 +28,28 @@ hintBox.innerHTML = hints[puzzleWord]
 for(i=0; i<checkWord.length; i++){
 	hiddenLetters = document.createElement('div') 
   hiddenLetters.setAttribute("class", "letter")
-	hiddenLetters.style.height = '10px'
-	hiddenLetters.style.width = '10px'
-	hiddenLetters.style.borderBottom = 'solid black'
-	hiddenLetters.style.display = 'inLineBlock'
+	hiddenLetters.style.height = '35px'
+	hiddenLetters.style.width = '35px'
+	hiddenLetters.style.borderBottom = 'solid white'
+	hiddenLetters.style.display = 'inLine-block'
+  hiddenLetters.style.marginLeft = '15px'
+  hiddenLetters.style.marginTop = '27px'
+  hiddenLetters.style.fontSize = '23'
+  hiddenLetters.style.textAlign = 'center'
 	wordToGuess.appendChild(hiddenLetters)
-  // hiddenLetters.innerHTML = " ";//  This is also kinda debugging
-}
+  }
 
-submit.addEventListener('click', function(){
-  usedLetters.innerHTML += playersLetter.value + ' '
+ submit.addEventListener('click', function(){
+    usedLetters.innerHTML += playersLetter.value + ' '
     
     for(i=0; i<checkWord.length; i++){
 
       if(playersLetter.value == (checkWord[i])){
-        console.log('in if#1')
-        console.log(playersLetter.value)
         var answerLetter = document.getElementsByClassName('letter')[i]
         answerLetter.innerHTML = playersLetter.value
-        console.log(answerLetter)
         wordLife-- 
        }
-       if(wordLife <= 0){
+      if(wordLife <= 0){
         playerMsg.innerHTML = winMessage
        }
       
@@ -55,14 +57,21 @@ submit.addEventListener('click', function(){
 
     function scoreLetter(value){
       return (checkWord.indexOf(value) === -1) ? false : true   
-  }
+    }
 
     if(scoreLetter(playersLetter.value)== false){
       playerLife--
-      alert("You have " + playerLife + ' ' + 'lives left')
+      alert("Wrong letter. You have " + playerLife + ' ' + 'lives left')
     }
     if(playerLife <= 0){
       playerMsg.innerHTML = loseMessage
+      pic2.style.height = '270px'
+      pic2.style.width = '270px'
+      pic2.style.marginTop = '-271px'
+      pic2.style.backgroundColor = 'red'
+      pic2.style.backgroundImage = "url('idiot.jpeg')";
+      pic1.style.backgroundImage = "url('losepic.jpg')";
+      
     }
 
 })
